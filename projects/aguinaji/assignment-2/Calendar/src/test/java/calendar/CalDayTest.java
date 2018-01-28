d6b23dcad445fef03065212b752385d373dbbd94
jimweldev <aguinaji@oregonstate.edu>
2018-01-28 15:09:50 -0800

    reached 80% for branch coverage for all classes

--- projects/aguinaji/assignment-2/Calendar/src/test/java/calendar/CalDayTest.java
+++ projects/aguinaji/assignment-2/Calendar/src/test/java/calendar/CalDayTest.java
@@ -44,6 +44,18 @@ public class CalDayTest {
 		 assertTrue(inc50westCalDay.contains(unityGames));
 		 assertTrue(inc50westCalDay.contains(musicalEvent));
 
+		 Appt invalidEvent = new Appt(10, 0,0,7,2018,
+				 "Musical","Musical Event");
+		 calday.addAppt(invalidEvent);
+		 assertFalse(calday.getAppts().contains(invalidEvent));
+
+		 Appt service = new Appt(2, 1,27,7,2018,
+				 "Service","Service Event");
+
+		 calday.addAppt(service);
+		 assertTrue(calday.getAppts().get(0).equals(service));
+
+
 	 }
 
 	@Test
@@ -54,6 +66,18 @@ public class CalDayTest {
 		String inc50westS = calday.toString();
 
 		assertTrue(inc50westS.contains("7/27/2018"));
+
+		Appt unityGames = new Appt(8, 1,27, 7,2018,
+				"Unity Games", "Unity Sports Games");
+
+		calday.addAppt(unityGames);
+		calday.addAppt(unityGames);
+		calday.addAppt(unityGames);
+
+		assertTrue(calday.toString().contains("Unity Games"));
+
+		CalDay calday2 = new CalDay();
+		assertTrue(calday2.toString().matches(""));
 
 	}
 
@@ -67,6 +91,7 @@ public class CalDayTest {
 		CalDay calday2 = new CalDay();
 
 		assertEquals(null, calday2.iterator());
+
 	}
 
 	 //add more unit tests as you needed
